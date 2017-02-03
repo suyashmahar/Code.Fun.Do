ad7de77c05dca139f8b44bf274b6d52b829f3f5a
iamharsshit <harshit.bansalec@gmail.com>
2017-02-03 23:16:03 +0530

    Detail Activity of event and campaign

---
+++ app/src/main/java/com/android/example/neighbours/CampaignDetail.java
@@ -0,0 +1,13 @@
+package com.android.example.neighbours;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CampaignDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campaign_detail);
+    }
+}
