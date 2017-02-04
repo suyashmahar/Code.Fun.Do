58c64b9713c2005b97ce654d503c3991366dfa15
iamharsshit <harshit.bansalec@gmail.com>
2017-02-05 01:10:44 +0530

    Bundle

--- app/src/main/java/com/android/example/neighbours/NotificationDetail.java
+++ app/src/main/java/com/android/example/neighbours/NotificationDetail.java
@@ -1,13 +1,31 @@
+// NotificationTitle,NotificationTime,NotificationDescription
 package com.android.example.neighbours;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NotificationDetail extends AppCompatActivity {
-
+    TextView notificationDetailTitle,notificationDetailTime,notificationDetailDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_detail);
+
+        notificationDetailTitle=(TextView)findViewById(R.id.notification_detail_title);
+        notificationDetailTime=(TextView)findViewById(R.id.notification_detail_time);
+        notificationDetailDescription=(TextView)findViewById(R.id.notification_detail_description);
+
+        Bundle extras=getIntent().getExtras();
+        if(extras!=null){
+            String notificationTitle=extras.getString("NotificationTitle");
+            String notificationTime=extras.getString("NotificationTime");
+            String notificationDescription=extras.getString("NotificationDescription");
+
+            notificationDetailTitle.setText(notificationTitle);
+            notificationDetailTime.setText(notificationTime);
+            notificationDetailDescription.setText(notificationDescription);
+
+        }
     }
 }
