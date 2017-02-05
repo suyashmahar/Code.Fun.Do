858444c6316ee9681a8eba8bed58985305b11d3b
jaskirat18 <amritjaskirat@gmail.com>
2017-02-05 16:43:02 +0530

    Modified create_notification.xml/.java

--- app/src/main/java/com/android/example/neighbours/create_notification.java
+++ app/src/main/java/com/android/example/neighbours/create_notification.java
@@ -2,12 +2,25 @@ package com.android.example.neighbours;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class create_notification extends AppCompatActivity {
+public class create_notification extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notification);
+
+        Button save=(Button) findViewById(R.id.confirm);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
