c2ca7d557c6971a8da1c0a92fe41f7a860fbc069
jaskirat18 <amritjaskirat@gmail.com>
2017-02-07 01:03:56 +0530

    Add good order complaint

--- app/src/main/java/com/android/example/neighbours/CreateComplaint.java
+++ app/src/main/java/com/android/example/neighbours/CreateComplaint.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class CreateComplaint extends AppCompatActivity
 {
@@ -46,8 +48,20 @@ public class CreateComplaint extends AppCompatActivity
         //method to confirm complaint
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
 
+                EditText details=(EditText) findViewById(R.id.message_complaint);
+                EditText name=(EditText) findViewById(R.id.complaint_name);
+
+                Uploader uploader = new Uploader(getApplicationContext());
+                //uploader.createAndPushNotification(new NotificationItem(detailsTextView.getText().toString(), "12:00", titleTextView.getText().toString()));
+
+                //uploader.createAndPushComplaints(new Complaint());
+                Complaint complaint=new Complaint("sample_user",details.getText().toString(),"sample_time",name.getText().toString());
+                uploader.createAndPushComplaints(complaint);
+                Toast.makeText(getApplicationContext(), "Complaint sent", Toast.LENGTH_LONG).show();
+                finish();
             }
         });
     }
